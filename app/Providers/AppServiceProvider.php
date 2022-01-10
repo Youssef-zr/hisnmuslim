75fdf505c61285c94c2f925539fe7aff9641f128
Youssef-Zr <yn-neinaa@hotmail.com>
2022-01-10 12:11:35 +0100

    add secure url https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Env;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') == "production") {
+        if (ENV::get('APP_ENV') == "production") {
             URL::forceScheme('https');
         }
     }
